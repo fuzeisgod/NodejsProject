b4b340f0137cc66d72cb999e672c0942c8460706
fuzzy <17826806525@163.com>
2019-08-05 17:11:37 +0800

    art-template

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var path = require('path')
 
 var app = express()
 
+
 app.use('/public', express.static(path.join(__dirname, './public')))
 app.use('/node_modules', express.static(path.join(__dirname, './node_modules')))
 
+
+app.set('views', path.join(__dirname, './views')) // 默认就是 ./views 目录
+app.engine('html', require('express-art-template'))
+
+
 app.get('/', function (req, res) {
-    res.send('Hello world')
+    res.render('index.html', {
+        name: 'czx'
+    })
 })
 
 app.listen(3000, function () {
