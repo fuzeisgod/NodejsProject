3540258e0807be977dd8f9c5204f8260a97b2ec9
fuzzy <17826806525@163.com>
2019-08-08 17:28:38 +0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 导入所需模块
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
 
 // 导入路由文件
 var router = require('./router.js')
@@ -15,6 +16,10 @@ app.use('/node_modules', express.static(path.join(__dirname, './node_modules')))
 // 配置 art-template
 app.set('views', path.join(__dirname, './views')) // 默认就是 ./views 目录
 app.engine('html', require('express-art-template'))
+
+// 配置解析表单 POST 请求体插件(body-parser)
+app.use(bodyParser.urlencoded({ extend: false }))
+app.use(bodyParser.json())
 
 // 挂载路由
 app.use(router)
--- router.js
+++ router.js
@@ -1,26 +1,59 @@
 var express = require('express')
+var User = require('./models/user.js')
 
 // 创建一个路由容器
 var router = express.Router()
 
-router.get('/', function(req, res){
+router.get('/', function (req, res) {
     res.render('index.html')
 })
 
-router.get('/login', function(req, res){
+router.get('/login', function (req, res) {
     res.render('login.html')
 })
 
-router.post('/login', function(req, res){
-    
+router.post('/login', function (req, res) {
+
 })
 
-router.get('/register', function(req, res){
+router.get('/register', function (req, res) {
     res.render('register.html')
 })
 
-router.post('/register', function(req, res){
-    
+router.post('/register', function (req, res) {
+    // 1.获取表单提交的数据
+    //   req.body
+    // 2.操作数据库
+    //   判断该用户是否存在
+    //   如果不存在，注册新建用户
+    // 3.发送响应
+    var body = req.body
+    User.findOne({
+        $or: [
+            { email: body.email },
+            { nickname: body.nickname }
+        ]
+    }, function (err, data) {
+        if (err) {
+            return res.status(500).json({
+                success: false,
+                message: '服务端错误'
+            })
+        }
+        if (data) {
+            // 邮箱或者昵称已存在
+            return res.status(200).json({
+                success: true,
+                message: '邮箱或者密码已存在'
+            })
+        }
+        // Express 提供了一个响应方法: json
+        // 该方法接收一个对象作为参数，它会自动帮你把对象转为字符串再发送给浏览器
+        res.status(200).json({
+            success: true,
+            message: 'ok'
+        })
+    })
 })
 
 
