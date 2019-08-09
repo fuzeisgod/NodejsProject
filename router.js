da1709cc19b1e2b5766a1cc61a807b437f2c6e10
fuzzy <17826806525@163.com>
2019-08-09 17:08:25 +0800

    。

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var User = require('./models/user.js')
+var md5 = require('blueimp-md5')
 
 // 创建一个路由容器
 var router = express.Router()
@@ -36,23 +37,36 @@ router.post('/register', function (req, res) {
     }, function (err, data) {
         if (err) {
             return res.status(500).json({
-                success: false,
-                message: '服务端错误'
+                err_code: 500,
+                message: 'Server error.'
             })
         }
         if (data) {
             // 邮箱或者昵称已存在
             return res.status(200).json({
-                success: true,
-                message: '邮箱或者密码已存在'
+                err_code: 1,
+                message: 'email or nickname already exists.'
             })
         }
-        // Express 提供了一个响应方法: json
-        // 该方法接收一个对象作为参数，它会自动帮你把对象转为字符串再发送给浏览器
-        res.status(200).json({
-            success: true,
-            message: 'ok'
+        // 对密码进行 md5 重复加密
+        body.password = md5(md5(body.password))
+        
+        new User(body).save(function (err, user) {
+            if (err) {
+                return res.status(500).json({
+                    err_code: 500,
+                    message: 'Server error.'
+                })
+            }
+            // Express 提供了一个响应方法: json
+            // 该方法接收一个对象作为参数，它会自动帮你把对象转为字符串再发送给浏览器
+            res.status(200).json({
+                err_code: 0,
+                message: 'ok'
+            })
         })
+
+
     })
 })
 
